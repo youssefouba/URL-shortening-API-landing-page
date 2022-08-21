e07546bae652d85120dd782edba89e7a06e1ea29
Youssef OUBAIDA <youssefoubaida1992@gmail.com>
2022-08-21 13:51:51 +0100

    beta4

--- src/components/Advancstat.js
+++ src/components/Advancstat.js
@@ -1,4 +1,7 @@
 import React from "react";
+import reco from "../images/icon-brand-recognition.svg";
+import deta from "../images/icon-detailed-records.svg";
+import cust from "../images/icon-fully-customizable.svg";
 
 const Advancstat = () => {
   return (
@@ -15,7 +18,7 @@ const Advancstat = () => {
         <div className="addetails">
           <div className="advbox">
             <div className="boxicon">
-            <img src="images/icon-brand-recognition.svg" alt="Brand Recognition"/>
+            <img src={reco} alt="Brand Recognition"/>
             </div>
             <h3>Brand Recognition</h3>
             <p>Boost your brand recognition with each click. Generic links don’t mean a thing. Branded links help instil confidence in your content.</p>
@@ -23,14 +26,14 @@ const Advancstat = () => {
       
           <div className="advbox box2">
             <div className="boxicon">
-            <img src="images/icon-Detailed-Records.svg" alt="Brand Recognition"/>
+            <img src={deta} alt="Brand Recognition"/>
             </div>
             <h3>Detailed Records</h3>
             <p>Gain insights into who is clicking your links. Knowing when and where people engage with your content helps inform better decisions..</p>
           </div>
           <div className="advbox box3">
             <div className="boxicon">
-            <img src="images/icon-Fully-Customizable.svg" alt="Brand Recognition"/>
+            <img src={cust} alt="Brand Recognition"/>
             </div>
             <h3>Fully Customizable</h3>
             <p>Improve brand awareness and content discoverability through customizable links, supercharging audience engagement.</p>
